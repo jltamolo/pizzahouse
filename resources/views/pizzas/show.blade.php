66e15c78793d83e5dcbc6e987e6735fdf8efe07e
jltamolo <jltamolo@gmail.com>
2020-11-24 12:29:14 +0300

    stored an array as json file in mysql

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -5,6 +5,13 @@
     <h1>Order for - {{$pizza->name}}</h1>
     <p class="type">Type - {{$pizza->type}}</p>
     <p class="base">Base - {{$pizza->base}}</p>
+    <p class="toppings">Extra toppings:</p>
+    <ul>
+        @foreach($pizza->toppings as $topping)
+        <li>{{$topping}}
+        @endforeach</li>
+    </ul>
+
     
 </div>
 <a href="/pizzas" class="back"><-Back to all pizzas</a>
